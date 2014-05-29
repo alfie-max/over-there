355733870c10a9af16cdf32d105323759243dccc
jasonklein <jasonklein@gmail.com>
2014-05-29 22:38:58 +0100

    Add Borough model and rake task to pull data from borough_data CSV file to instantiate new borough instances.

---
+++ app/models/borough.rb
@@ -0,0 +1,3 @@
+class Borough < ActiveRecord::Base
+  attr_accessible :anxiety_score, :average_age, :crime_rates_per_capita, :employment_rate, :first_largest_immigrant_group, :gross_annual_pay, :happiness_score, :inner_outer, :life_satisfaction_score, :name, :percent_cycling_adults, :percent_greenspace, :political_control, :population, :population_density, :population_percent_foreign, :population_percent_senior, :population_percent_working, :population_percent_youth, :second_largest_immigrant_group, :third_largest_immigrant_group, :worthwhileness_score
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,33 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20140529212925) do
+
+  create_table "boroughs", :force => true do |t|
+    t.string   "name"
+    t.string   "inner_outer"
+    t.decimal  "population"
+    t.decimal  "population_density"
+    t.decimal  "average_age"
+    t.decimal  "population_percent_youth"
+    t.decimal  "population_percent_working"
+    t.decimal  "population_percent_senior"
+    t.decimal  "population_percent_foreign"
+    t.string   "first_largest_immigrant_group"
+    t.string   "second_largest_immigrant_group"
+    t.string   "third_largest_immigrant_group"
+    t.decimal  "employment_rate"
+    t.decimal  "gross_annual_pay"
+    t.decimal  "crime_rates_per_capita"
+    t.decimal  "percent_greenspace"
+    t.decimal  "percent_cycling_adults"
+    t.decimal  "life_satisfaction_score"
+    t.decimal  "worthwhileness_score"
+    t.decimal  "happiness_score"
+    t.decimal  "anxiety_score"
+    t.string   "political_control"
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+  end
 
 end
