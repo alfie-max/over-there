020a0455d184831f9f53afa02276d243ae58cc9a
ScottyGams <ScottyGammon@gmail.com>
2014-05-29 20:27:58 +0100

    going home for the night

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,7 +2,7 @@ class ClientsController < ApplicationController
   
   def index
     c = Client.new
-    @clients = c.search_venues(:ll => '51.5224405,-0.1094789', :query => 'hipster')
+    @clients = c.search_venues_by_tip(:ll => '51.5224405,-0.1094789', :query => 'hipster', :limit => 20)
   end
 
 end
