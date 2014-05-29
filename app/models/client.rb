94424cbedf42fcbe70737836176dff37f14a5477
ScottyGams <ScottyGammon@gmail.com>
2014-05-29 16:59:31 +0100

    putting in client model

---
+++ app/models/client.rb
@@ -0,0 +1,5 @@
+Class Client < Foursquare2::Client
+
+def initialize
+  super((:client_id => 'LRDVJ31Z3N2GREGOT5X4UA1BASUZJGW5VNMUOL3GXNRAKM3P', :client_secret => 'R34UO3K0KBD12KFEOCNZLJJCPSM2TF1PMTD4Q11SY431KN1L'))
+end
