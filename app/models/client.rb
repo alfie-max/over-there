6677c0a024ec15fda9670d2f12a5508fd18bf0c1
ScottyGams <ScottyGammon@gmail.com>
2014-05-29 17:14:49 +0100

    working api

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,7 @@
-Class Client < Foursquare2::Client
+class Client < Foursquare2::Client
 
-def initialize
-  super((:client_id => 'LRDVJ31Z3N2GREGOT5X4UA1BASUZJGW5VNMUOL3GXNRAKM3P', :client_secret => 'R34UO3K0KBD12KFEOCNZLJJCPSM2TF1PMTD4Q11SY431KN1L'))
+  def initialize
+    super(:client_id => 'LRDVJ31Z3N2GREGOT5X4UA1BASUZJGW5VNMUOL3GXNRAKM3P', :client_secret => 'R34UO3K0KBD12KFEOCNZLJJCPSM2TF1PMTD4Q11SY431KN1L', :api_version => '20120505')
+  end
+
 end
