999e750fe59a91bbef886de671995819d1a8bab3
paulvarga23 <paul_varga@me.com>
2014-05-29 19:25:11 +0100

    method for zoopla request

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,4 +6,19 @@ class MainController < ApplicationController
   def show
   end
 
+  def search_zoopla
+    respond_to do |format|
+      @request = 'W149AB'
+
+      if @request && @request > ""
+        @postcode = @request.downcase.tr(" ", "+")
+
+        url ="http://api.zoopla.co.uk/api/v1/average_sold_prices.json?postcode=#{postcode}&output_type=outcode&area_type=postcodes&api_key=#{ENV['ZOOPLA_API_KEY']}"
+        html = HTTParty.get(url)
+        @hash = JSON(html)
+
+      end
+    end
+  end
+
 end
