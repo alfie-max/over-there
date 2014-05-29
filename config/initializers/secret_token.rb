0dc18cbac43c2d4f892f1afc991888c6d806ccbb
daniel1bell <daniel1bell@me.com>
2014-05-29 16:12:11 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OverThereApp::Application.config.secret_token = 'e63615d04296b1b684b5ffcea7a3ee81f75bcecc53772ee68b136caa0e8a2fece5328e2a0642b1c772acae809588b932096511669d259ddac2daa2ffc93a056f'
