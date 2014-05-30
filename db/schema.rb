754134ddf9353f69f5d3147f70105a2330e6b0ad
jasonklein <jasonklein@gmail.com>
2014-05-30 10:21:08 +0100

    Fix merge conflicts and combine borough model data.

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,45 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140529212925) do
+ActiveRecord::Schema.define(:version => 20140530090753) do
 
   create_table "boroughs", :force => true do |t|
     t.string   "name"
     t.string   "inner_outer"
-    t.decimal  "population"
-    t.decimal  "population_density"
-    t.decimal  "average_age"
-    t.decimal  "population_percent_youth"
-    t.decimal  "population_percent_working"
-    t.decimal  "population_percent_senior"
-    t.decimal  "population_percent_foreign"
+    t.float    "population"
+    t.float    "population_density"
+    t.float    "average_age"
+    t.float    "population_percent_youth"
+    t.float    "population_percent_working"
+    t.float    "population_percent_senior"
+    t.float    "population_percent_foreign"
     t.string   "first_largest_immigrant_group"
     t.string   "second_largest_immigrant_group"
     t.string   "third_largest_immigrant_group"
-    t.decimal  "employment_rate"
-    t.decimal  "gross_annual_pay"
-    t.decimal  "crime_rates_per_capita"
-    t.decimal  "percent_greenspace"
-    t.decimal  "percent_cycling_adults"
-    t.decimal  "life_satisfaction_score"
-    t.decimal  "worthwhileness_score"
-    t.decimal  "happiness_score"
-    t.decimal  "anxiety_score"
+    t.float    "employment_rate"
+    t.float    "gross_annual_pay"
+    t.float    "crime_rates_per_capita"
+    t.float    "percent_greenspace"
+    t.float    "percent_cycling_adults"
+    t.float    "life_satisfaction_score"
+    t.float    "worthwhileness_score"
+    t.float    "happiness_score"
+    t.float    "anxiety_score"
     t.string   "political_control"
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "location"
+  end
+
+  create_table "postcodes", :force => true do |t|
+    t.string   "postcode"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "borough_id"
   end
 
 end
